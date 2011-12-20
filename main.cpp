f9e2712a638f5d3d90497a30b5ec58afb422c88a
David C. Lonie <loniedavid@gmail.com>
2011-12-20 16:48:57 -0500

    Add fullscreen option.

--- main.cpp
+++ main.cpp
@@ -3,21 +3,41 @@
 #include <QtGui/QPainter>
 #include <QtGui/QWidget>
 
+#include <QtCore/QDebug>
+
 #include <flockwidget.h>
+
+#include <string.h>
 
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
 
+  bool fullscreen = false;
+  if (argc >= 2) {
+    int argInd = 0;
+    while (char *arg = argv[argInd++]) {
+      if (std::strcmp(arg, "-f") == true) {
+        fullscreen = true;
+      }
+    }
+  }
+
   QMainWindow mw;
   QWidget *target = new FlockWidget(&mw);
   mw.setCentralWidget(target);
-  QRect rect = mw.geometry();
-  rect.setWidth(600);
-  rect.setHeight(600);
-  mw.setGeometry(rect);
 
-  mw.show();
+  if (fullscreen) {
+    mw.showFullScreen();
+  }
+  else {
+    QRect rect = mw.geometry();
+    rect.setWidth(600);
+    rect.setHeight(600);
+    mw.setGeometry(rect);
+    mw.show();
+  }
+
 
   return app.exec();
 }
